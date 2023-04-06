441db148856f6624c98197de5c2e1a62fbfdd656
Jacobtread <jacobtread@gmail.com>
2023-04-06 16:43:05 +1200

    Initial source

---
+++ AdvMald.cpp
@@ -0,0 +1,12 @@
+﻿// AdvMald.cpp : Defines the entry point for the application.
+//
+
+#include "AdvMald.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "This is a message going to stdout" << endl;
+	return 0;
+}
